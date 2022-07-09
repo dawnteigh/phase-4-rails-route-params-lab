5764c9b4f218517aa4bdd2abc0b9f3d24124ca24
Donte Ladatto <donte279@gmail.com>
2022-07-09 16:30:13 -0500

    commit tree 60c45d366263268dc71b102ab8d294011e440a29

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,12 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    render json: Student.find(params[:id])
+  end
+
+  def search
+    render json: Student.all.filter{ |s| s.to_s.include?(params[:name])}
+  end
+
 end
